7c6543c8af2da62066c591ce0a4b9abb1941f64b
graeme81 <g.cameron@hotmail.co.uk>
2019-05-07 17:42:57 +0100

    little bit done, movement, player and platforms

---
+++ TheGame/src/game/Harkins.java
@@ -0,0 +1,18 @@
+package game;
+
+import java.awt.*;
+
+public class Harkins {
+	
+	
+	
+	public Harkins(int h){
+		int height = h/20;
+		int width = height/2;
+		
+		Rectangle head = new Rectangle(height/3, width);
+		Rectangle body = new Rectangle(2*(height/3), width);
+		
+	}
+
+}
